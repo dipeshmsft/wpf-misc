5c31450eb5732d768dcca3410d989bc767ab91c6
Dipesh Kumar <dipeshkumar@microsoft.com>
2025-05-26 12:42:59 +0530

    Added GridView Fluent App

---
+++ apps/fluent-test-apps/GridViewApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GridViewApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+    public class MainViewModel
+    {
+        public class Car
+        {
+            public string? Make { get; set; }
+
+            public string? Model { get; set; }
+        }
+
+        public ObservableCollection<Car> Cars { get; set; } = [];
+
+        public MainViewModel()
+        {
+            Cars.Add(new Car() { Make = "Toyota", Model = "Corolla" });
+            Cars.Add(new Car() { Make = "Honda", Model = "Civic" });
+        }
+    }
+}
---
+++ apps/fluent-test-apps/GridViewApp/ViewIsGridViewConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace GridViewApp
+{
+    internal class ViewIsGridViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Returns true if value is a GridView, otherwise false
+            return value is GridView;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
