7ee5622d6131b4cc06ee97759e0a27eab551336f
Dipesh Kumar <85861525+dipeshmsft@users.noreply.github.com>
2025-06-03 13:37:24 +0530

    Added some more examples - GridView (#23)
    * Added samples apps to test ComboBox styles
    
    * Fixed triggers for ComboBox style
    
    * Added some more changes
    
    * Added GridView Fluent App
    
    * Added more apps for Fluent testing
    
    ---------
    
    Co-authored-by: Dipesh Kumar <dipeshkrj14@gmail.com>

---
+++ apps/fluent-test-apps/MultiThreadedFluentApp/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiThreadedFluentApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MTLightWindow_Click(object sender, RoutedEventArgs e)
+        {
+            tm = ThemeMode.Light;
+            StartNewThreadedWindow();
+        }
+
+        private void MTDarkWindow_Click(object sender, RoutedEventArgs e)
+        {
+            tm = ThemeMode.Dark;
+            StartNewThreadedWindow();
+        }
+
+        private void MTSystemWindow_Click(object sender, RoutedEventArgs e)
+        {
+            tm = ThemeMode.System;
+            StartNewThreadedWindow();
+
+        }
+
+        private static void StartNewThreadedWindow()
+        {
+            var thread = new Thread(StartOnThread);
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+
+        private static void StartOnThread(object? obj)
+        {
+            var window = new MainWindow
+            {
+                ThemeMode = tm
+            };
+            window.Show();
+            System.Windows.Threading.Dispatcher.Run();
+        }
+
+        static ThemeMode tm;
+    }
+}
